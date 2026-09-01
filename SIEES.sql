54cb3677e33850d38da8d6c60dfe8e8d7f9c528e
Trg7470 <222310291@itslerdo.edu.mx>
2026-09-01 11:53:47 -0600

    Update SIEES.sql

--- SIEES.sql
+++ SIEES.sql
@@ -562,3 +562,30 @@ SET character_set_client = @saved_cs_client;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2026-08-26 10:28:11
+
+CREATE OR REPLACE VIEW vw_expedientes AS
+SELECT
+    cd.Id_Carpeta,
+    CONCAT(
+        dp.Nombre, ' ',
+        dp.Apellido_Paterno, ' ',
+        dp.Apellido_Materno
+    ) AS Nombre_Completo,
+    dp.CURP,
+    dp.Telefono,
+    u.Nombre AS Adscripcion,
+    c.Nombre AS Carrera,
+    e.Nombre AS Escuela
+FROM Carpetas_Drive cd
+INNER JOIN Adscripciones a
+    ON a.Id_Adscripcion = cd.Id_Adscripcion
+INNER JOIN Datos_Personales dp
+    ON dp.Id_Persona = a.Id_Persona_Adscripcion
+INNER JOIN Unidades u
+    ON u.Id_Unidad = a.Id_Unidad_Adscripcion
+INNER JOIN Datos_Escolares de
+    ON de.Id_Persona_D_Escolar = dp.Id_Persona
+INNER JOIN Carreras c
+    ON c.Id_Carrera = de.Id_Carrera_D_Escolar
+INNER JOIN Escuelas e
+    ON e.Id_Escuela = de.Id_Escuela_D_Escolar;
