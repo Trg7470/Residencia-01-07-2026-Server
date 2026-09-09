ca663dcdf0f61ad05916a09e8d870dd0ce5d0291
Sarai Flores <222310202@itslerdo.edu.mx>
2026-09-09 13:50:25 -0600

    formato presentacion
    plantilla modificada, carta de presentacion datos modificados

--- api/src/services/documentos/datos/carta_presentacion.datos.js
+++ api/src/services/documentos/datos/carta_presentacion.datos.js
@@ -2,17 +2,20 @@
 function ObtenerDatosPresentacion(){
     return {
         Fecha_Actual: "01 de agosto de 2025",
-        Dirigido_A: "DR. JOSÉ ALBERTO ESCOBEDO REYES",
-        Ocupacion1: "Jefe del Área Operativa No. 2",
-        Ocupacion2: "Director del C.S.U. Dr. Roberto García Sosa",
-        Alumno: "M.P.S.S GALARZA RODRIGUEZ GRECIA ALESSANDRA",
+        Abreviatura_Destinatario: "DR",
+        Dirigido_A: "JOSÉ ALBERTO ESCOBEDO REYES",
+        Ocupacion: `Jefe del Área Operativa No. 2 y
+Director del C.S.U. Dr. Roberto García Sosa`,
+        Abreviatura_Alumno:"MPSS",       
+        Alumno: "GALARZA RODRIGUEZ GRECIA ALESSANDRA",
         Carrera: "MEDICO CIRUJANO",
-        Universidad: "Facultad de Ciencias de la Salud de la Universidad Juárez del Estado de Durango",
+        Escuela: "Facultad de Ciencias de la Salud de la Universidad Juárez del Estado de Durango",
         Tipo_Participacion: "SERVICIO SOCIAL",
         Lugar_Adscripcion: "C.S.R. CD. JUÁREZ, DGO.",
         Lugar_Comision: "C.S.U. LAS LUISAS",
         Fecha_Inicio: "10 de noviembre de 2025",
         Fecha_Termino: "31 de diciembre de 2025",
+        Abreviatura_Encargado: "DR",
         Encargado: "PEDRO ANTONIO FACIO TORRES" 
     };
 }
